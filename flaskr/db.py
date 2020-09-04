4ae9ce38c5c2319c1655922599c77d10bd803a65
s-mingzhe.yu <tylov.ohno@gmail.com>
2020-09-04 17:06:09 +0900

    test wefwef

--- flaskr/db.py
+++ flaskr/db.py
@@ -31,7 +31,7 @@ def init_db():
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    """Clear the existing date and create new tables."""
     init_db()
     click.echo('Initialized the database.')
 
